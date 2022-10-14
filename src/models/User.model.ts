6b19fb9f6ba1e00c50630e812e68b96ca6af960b
Oleg Kireev <okireev@ubic.tech>
2022-10-14 10:39:29 +0400

    @/models/{Role, User}: create

---
+++ src/models/User.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model} from 'mongoose';
+
+const User = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [{
+    type: String,
+    ref: 'Role'
+  }]
+});
+
+export const UserModel = model('User', User);
