468e4d9b616dc1e541dc5183306c37e0db48db21
Oleg Kireev <okireev@ubic.tech>
2022-10-14 12:20:28 +0400

    @/controllers/auth: implement a base sign up logic

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,19 +1,62 @@
 import { Request, Response } from "express";
+import { UserModel } from '../models/User.model';
+import { RoleModel } from '../models/Role.model';
+import { hash } from 'bcryptjs';
+
+const PASSWORD_SALT = 7;
+
+interface ISignUpRequestBody {
+  username: string;
+  password: string;
+}
+
+interface ISignUpResponseBody {
+  message: string;
+}
 
 class AuthController {
-  async signUp(req: Request, res: Response) {
+  async signUp(
+    req: Request<null, null, ISignUpRequestBody>,
+    res: Response<ISignUpResponseBody>
+  ) {
     try {
-      
+      const { username, password } = req.body;
+      const alreadyRegistredUser = await UserModel.findOne({ username });
+
+      if (alreadyRegistredUser) {
+        return res
+          .status(400)
+          .json({ message: `User with username: ${username} has already registred`});
+      }
+
+      const hashedPassword = await hash(password, PASSWORD_SALT);
+
+      const newUserRole = await RoleModel.findOne({value: 'USER'});
+      console.log(newUserRole);
+
+      const newUser = new UserModel({ 
+        username,
+        password: hashedPassword,
+        roles: [
+          newUserRole?.value
+        ],
+      });
+      await newUser.save();
+
+      return res.status(201).json({ message: 'Success registration'});
+
     } catch (err) {
       console.warn(err);
+      res.status(400).json({ message: 'Registartion error'});
     }
   }
 
   async login(req: Request, res: Response) {
       try {
-        
+        console.log('login');
       } catch (err) {
         console.warn(err);
+        res.status(400).json({ message: 'Login error'});
       }
   }
 
