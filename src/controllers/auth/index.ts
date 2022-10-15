e321d8232bac2d52fc4816f7220c07d405bd6ab1
Oleg Kireev <okireev@ubic.tech>
2022-10-15 10:17:18 +0400

    @/controllers/auth: remove getUsers

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,11 +1,9 @@
 import { signUp } from "./sign-up";
 import { login} from "./login";
-import { getUsers } from "./get-users";
 
 class AuthController {
   signUp = signUp
   login = login
-  getUsers =  getUsers
 }
 
 export const authController = new AuthController();
